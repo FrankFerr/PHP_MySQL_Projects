bf18c87a3398756645835b9cd63bcc58037ee81a
FrankFerr <francescoferrante787@gmail.com>
2022-04-10 22:07:08 +0200

    update PHP

---
+++ Esercizi/FileSystem/demo.php
@@ -0,0 +1,27 @@
+<style>*{font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;font-size:1.2rem;}</style>
+<?php
+
+echo __DIR__.'<br>';
+echo __FILE__.'<br>';
+
+
+//CREATE DIRECTORY **************************************
+
+//crea una cartella nella directory corrente
+// mkdir('Prova');
+
+//settando recursive a true va a creare tutte le cartelle del percoso che abbiamo specificato
+// mkdir('Recursive\Prova', recursive:true);
+
+
+//RENAME DIRECTORY ****************************************
+// rename('Recursive\Prova', 'Recursive\my_recursive');
+
+
+//REMOVE DIRECTORY **************************************
+// rmdir('Prova');
+
+
+
+
+?>
